cc0f487de07c2e8b58b384459eb9c05baa5936b2
badugumanivarshith-mrv <badugumanivarshith@gmail.com>
2026-08-31 18:40:16 +0530

    Ignore build artifacts

--- backend/dist/modules/health/health.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const health_controller_1 = require("./health.controller");
-const router = (0, express_1.Router)();
-const controller = new health_controller_1.HealthController();
-router.get('/', controller.getHealth);
-exports.default = router;
